ea98d56d1482b7b4d7a4760e2e15968fad938737
Ben <akhlifa@students.beaminstitute.org>
2024-05-16 22:55:57 +0200

    new

---
+++ README.md
@@ -0,0 +1,17 @@
+
+### Features
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+Install required dependencies
+<pre>yarn install</pre>
+Start the server
+<pre>yarn start</pre>
+
+### Thanks
+
+
---
+++ src/content_option.js
@@ -0,0 +1,154 @@
+const logotext = "BEN";
+const meta = {
+    title: "Ben khlifa",
+    description: "I’m BEN_ Full stack devloper,currently in Düsseldorf",
+};
+
+const introdata = {
+    title: "I’m Ben khlifa",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        
+    },
+    description: "I am a passionate Junior Web Developer with a strong foundation in HTML, CSS, and JavaScript, along with basic knowledge of server-side languages. My understanding of UI/UX principles, combined with excellent problem-solving and communication skills, allows me to create user-friendly and efficient websites. I hold a bachelor's degree in Computer Science and am eager to stay updated with emerging technologies. I thrive in collaborative environments and am dedicated to continuous learning and career advancement in web development.",
+    your_img_url: "https://unsplash.com/de/fotos/eine-person-die-eine-treppe-hinaufgeht-jz6pmFhRE6c",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a positive and enthusiastic Junior Web Developer with a solid foundation in HTML, CSS, and JavaScript. I excel in creating user-friendly websites and enjoy collaborating with teams. My problem-solving skills and eagerness to learn keep me motivated and adaptable in the ever-evolving tech landscape.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "berlin",
+        date: "2023",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Amestrdam",
+        date: "2024",
+    },
+    
+];
+
+const skills = [
+    
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Html & Css",
+        value : 80,
+    }
+    
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "  creating visually appealing and user-friendly digital interfaces. UI design handles the visual elements like layout and colors, while UX design ensures the product is intuitive and satisfying to use. Together, they enhance usability and create seamless digital experiences for users.",
+    },
+    
+    {
+        title: "Wordpress Design",
+        description: "Design involves creating and customizing websites using the WordPress platform. It focuses on developing visually appealing, responsive, and user-friendly sites tailored to specific needs. Designers use themes, plugins, and custom code to build unique layouts, ensuring optimal performance and functionality while maintaining brand consistency.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "SOON",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "SOON...",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "LOKO71273@gmail.com ",
+    YOUR_FONE: "+4917662315849",
+    description: "I’m thrilled about the opportunity to collaborate with you! Please feel free to contact me so we can discuss this exciting venture further. Looking forward to hearing from you soon! ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
